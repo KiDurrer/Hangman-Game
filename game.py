17d6d1be77c3c37d9467b346e3e700a02d0d2e18
bm20894 <milesboz@me.com>
2018-11-12 19:20:13 -0600

    moved hangmen into hangman.py

--- game.py
+++ game.py
@@ -4,158 +4,73 @@ TODO:
 [X] Setup Basics, Introduction
 [X] Program loop system to show if you guessed right letter
 [ ] Use larger list of words to choose from
-[ ] Display image of hangman
+[X] Display image of hangman
 [ ] Show letter guesses in color
 '''
+from hangman import HANGMAN
 import random
 import os
-
-HANGMAN = (
-    """
-    -----
-    |   |
-    |   0
-    |
-    |
-    |
-    |
-    |
-    |
-    --------
-    """,
-    """
-    -----
-    |   |
-    |   0
-    |  -+-
-    |
-    |
-    |
-    |
-    |
-    --------
-    """,
-    """
-    -----
-    |   |
-    |   0
-    | /-+-
-    |
-    |
-    |
-    |
-    |
-    --------
-    """,
-    """
-    -----
-    |   |
-    |   0
-    | /-+-\ 
-    |
-    |
-    |
-    |
-    |
-    --------
-    """,
-    """
-    -----
-    |   |
-    |   0
-    | /-+-\ 
-    |   | 
-    |   | 
-    |
-    |
-    |
-    --------
-    """,
-    """
-    -----
-    |   |
-    |   0
-    | /-+-\ 
-    |   | 
-    |   | 
-    |  | 
-    |  | 
-    |
-    --------
-    """,
-
-    """
-    -----
-    |   |
-    |   0
-    | /-+-\ 
-    |   | 
-    |   | 
-    |  | | 
-    |  | | 
-    |
-    --------
-    """)
 
 print('Welcome to Hang-man!')
 input('Press ENTER To Play')
 
 
 def randomword():
-    #test word list
-    randomwordlist = ['car','shop','man','blue','blueberry']
-    rword = random.choice(randomwordlist)
-    return rword
+	#test word list
+	randomwordlist = ['car','shop','man','blue','blueberry']
+	rword = random.choice(randomwordlist)
+	return rword
 
 def place_letters(wordmat, word, letter):
-    locations = [i for i, _ in enumerate(word) if word.startswith(letter, i)]
+	locations = [i for i, _ in enumerate(word) if word.startswith(letter, i)]
 
-    for loc in locations:
-        wordmat = wordmat[:loc] + letter + wordmat[loc+1:]
-    return wordmat
+	for loc in locations:
+		wordmat = wordmat[:loc] + letter + wordmat[loc+1:]
+	return wordmat
 
 
 def hangman(word):
-    print('DEBUG ANSWER: ' + word) #debug answer
+	print('DEBUG ANSWER: ' + word)
 
-    guessed = []
-    attempts = 7 
-    n = attempts
-    
-    wordmat = '_' * len(word)
-    blanks = ' '.join(wordmat)
-    
-    #main game loop
-    while attempts > 0:
-        print('=====================================')
-        print('Guessed letters: {}\nAttempts left: {}' \
-            .format(','.join(guessed), attempts))
-        print(blanks)
+	guessed = []
+	attempts = 7 
+	n = attempts
 
-        guess = input('Guess a letter: ')
-        
-        if guess in guessed:
-            print('You already guessed {}. Guess again!'.format(guess))
-            continue
+	wordmat = '_' * len(word)
+	blanks = ' '.join(wordmat)
 
-        if guess not in word:
-            print('{} is not in my word.'.format(guess))
-            guessed.append(guess)
-            attempts -= 1
-            print(HANGMAN[n - 1 - attempts])            
-            continue
+	#main game loop
+	while attempts > 0:
+		print('=====================================')
+		print('Guessed letters: {}\nAttempts left: {}' \
+		    .format(','.join(guessed), attempts))
+		print(blanks)
 
-        # guess is in word and hasn't been guessed already
-        guessed.append(guess)
-        wordmat = place_letters(wordmat, word, guess)
-        blanks = ' '.join(wordmat)
+		guess = input('Guess a letter: ')
 
-        if wordmat == word:
-            print('Congratulations! You guessed {}!'.format(word))
-            break
 
-    else:
-        print('Game over, you ran out of guesses. My word was {}'.format(word))
+		if guess in guessed:
+			print('You already guessed {}. Guess again!'.format(guess))
+
+		elif guess not in word:
+			print('{} is not in my word.'.format(guess))
+			guessed.append(guess)
+			attempts -= 1
+
+		else:
+			# guess is in word and hasn't been guessed already
+			guessed.append(guess)
+			wordmat = place_letters(wordmat, word, guess)
+			blanks = ' '.join(wordmat)
+
+			if wordmat == word:
+				print(HANGMAN[n - attempts])
+				print('Congratulations! You guessed {}!'.format(word))
+				break
+		print(HANGMAN[n - attempts])
+
+	else:
+		print('Game over, you ran out of guesses. My word was {}.'.format(word))
 
 
 print()
